0829f6b3394afc96bfe77a62dd340d67725409c3
vpnuser <vpnuser@KTH-LAP-118.ezest.local>
2022-05-13 18:53:15 +0530

    before sending to git stage-2 need some fixes

--- JS/app.js
+++ JS/app.js
@@ -2,10 +2,7 @@ let inputSearch = document.getElementById("inputSearch");
 let form = document.getElementsByTagName("form")
 var counter = 0
 
-// inputSearch.style.visibility = "hidden";
-// form.addEventListener('submit',(e) =>{
-//     e.preventDefault();
-// })
+
 inputSearch.style.visibility = "hidden"
 function displayInput(){
   
@@ -19,11 +16,3 @@ function displayInput(){
 }
 
 
-// var AllButtons = document.getElementsByTagName("button");
-
-// AllButtons.forEach(btn => {
-//     btn.addEventListener('click',() =>{
-//         alert('button clicked')
-//         btn.style.transform = "scale(0.98)";
-//     })
-// });
